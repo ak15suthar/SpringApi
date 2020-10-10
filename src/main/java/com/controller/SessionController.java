84f0a56039ceca3ab16fb3b84c4e79eef851557d
Tejas <tejas14all@gmail.com>
2020-10-10 19:23:29 +0530

    signup ready

--- src/main/java/com/controller/SessionController.java
+++ src/main/java/com/controller/SessionController.java
@@ -11,84 +11,110 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bean.DoctorProfileBean;
 import com.bean.LoginBean;
 import com.bean.ResponseBean;
 import com.bean.UserBean;
-import com.dao.SignupDao;
+import com.dao.SessionDao;
+import com.services.MailerService;
+import com.services.OtpService;
 
 @RestController
-public class SignupController {
-	
+public class SessionController {
+
 	@Autowired
-	SignupDao signupDao;
-	
-	
+	SessionDao sessionDao;
+
+	@Autowired
+	OtpService OtpService;
+	@Autowired
+	MailerService mailerService;
+
 	@PostMapping("/login")
-	public ResponseBean<UserBean> login(@RequestBody LoginBean loginBean){
-		
+	public ResponseBean<UserBean> login(@RequestBody LoginBean loginBean) {
+
 		ResponseBean<UserBean> responseBean = new ResponseBean<>();
-			
-		UserBean signupBean = signupDao.login(loginBean.getEmail(),loginBean.getPassword());
-		
+
+		UserBean signupBean = sessionDao.login(loginBean.getEmail(), loginBean.getPassword());
+
 		responseBean.setData(signupBean);
 		responseBean.setMsg("Login Successfully!!");
 		responseBean.setStatus(200);
-		
+
 		return responseBean;
 	}
-	
-	@PostMapping("/addSignup")
-	public ResponseBean<UserBean> addSignup(@RequestBody UserBean signupBean){
+
+	@PostMapping("/signup")
+	public ResponseBean<UserBean> signup(@RequestBody UserBean userBean) {
+
+		userBean.setOtp(OtpService.generateOtp());
+		mailerService.sendOtpForUserVerification(userBean);
+		sessionDao.insertUser(userBean);
+
+		ResponseBean<UserBean> responseBean = new ResponseBean<>();
+
+		responseBean.setData(userBean);
+		responseBean.setMsg("user successfully signup!!");
+		responseBean.setStatus(200);
+
+		return responseBean;
+	}
+
+	@PostMapping("/doctorsignup")
+	public ResponseBean<UserBean> doctorSignup(@RequestBody DoctorProfileBean doctorProfileBean) {
+
+		doctorProfileBean.setStatus(UserBean.KYC_DOCTOR);
+		doctorProfileBean.setStatusReason("Your KYS is pending Our Team Will Contact You Soon..");	
+		mailerService.sendDoctorRegisterMail(doctorProfileBean);
 		
-		signupDao.insertSignup(signupBean);
 		
 		ResponseBean<UserBean> responseBean = new ResponseBean<>();
-		
-		responseBean.setData(signupBean);
-		responseBean.setMsg("Signup Inserted!!");
+
+		responseBean.setData(doctorProfileBean);
+		responseBean.setMsg("user successfully signup!!");
 		responseBean.setStatus(200);
-		
+
 		return responseBean;
 	}
-	
+
 	@GetMapping("/listSignup")
-	public ResponseBean<List<UserBean>> listSignup(){
-		
-		List<UserBean> signupBean = signupDao.listSignup();
-		
+	public ResponseBean<List<UserBean>> listSignup() {
+
+		List<UserBean> signupBean = sessionDao.listSignup();
+
 		ResponseBean<List<UserBean>> responseBean = new ResponseBean<>();
-		
+
 		responseBean.setData(signupBean);
 		responseBean.setMsg("Signup List!!");
 		responseBean.setStatus(200);
-		
+
 		return responseBean;
 	}
-	
+
 	@PutMapping("updateSignup")
-	public ResponseBean<UserBean> updateSignup(UserBean signupBean){
-		
-		signupDao.updateSignup(signupBean);
-		
+	public ResponseBean<UserBean> updateSignup(UserBean signupBean) {
+
+		sessionDao.updateSignup(signupBean);
+
 		ResponseBean<UserBean> responseBean = new ResponseBean<>();
 		responseBean.setData(signupBean);
 		responseBean.setMsg("Signup Updated!!");
 		responseBean.setStatus(201);
-		
+
 		return responseBean;
 	}
-	
+
 	@DeleteMapping("deleteSignup/{userId}")
-	public ResponseBean<UserBean> deleteSignup(@PathVariable("userId") int userId){
-		
-		signupDao.deleteSignup(userId);
-		
+	public ResponseBean<UserBean> deleteSignup(@PathVariable("userId") int userId) {
+
+		sessionDao.deleteSignup(userId);
+
 		ResponseBean<UserBean> responseBean = new ResponseBean<>();
-		
+
 		responseBean.setMsg("Signup Deleted!!");
 		responseBean.setStatus(201);
-		
+
 		return responseBean;
-		
+
 	}
 }
